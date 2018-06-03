98edf8258518e658f174d6eb01f6fa8223b5abe2
ThomBSI <thomas@marquis.fr.cr>
2018-06-03 23:02:55 +0200

    36: quilifires, primary & profiles

--- src/main/java/guru/springframework/DiDemoApplication.java
+++ src/main/java/guru/springframework/DiDemoApplication.java
@@ -16,10 +16,9 @@ public class DiDemoApplication {
 
 		MyController controller = (MyController) ctx.getBean("myController");
 
-		controller.hello();
-
 		System.out.println(ctx.getBean(PropertyInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(GetterInjectedController.class).sayHello());
 		System.out.println(ctx.getBean(ConstructorInjectedController.class).sayHello());
+		System.out.println(controller.hello());
 	}
 }
---
+++ src/main/java/guru/springframework/services/PrimaryGreetingService.java
@@ -0,0 +1,16 @@
+package guru.springframework.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+@Profile("en")
+public class PrimaryGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "hello - primary service";
+    }
+}
